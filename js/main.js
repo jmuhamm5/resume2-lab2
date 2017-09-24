6a3645d072e9252acd79d1f995034dffdcdc9810
jmuhamm5 <apple_life15@yahoo.com>
2017-09-23 21:48:36 -0500

    HTML JS

--- js/main.js
+++ js/main.js
@@ -4,14 +4,11 @@ $(document).ready(function(){
 
     // Next slide
     $('.carousel').carousel('next');
-    $('.carousel').carousel('next', 3); // Move next n times.
+    $('.carousel').carousel('next', 4); // Move next n times.
 
     // Previous slide
     $('.carousel').carousel('prev');
     $('.carousel').carousel('prev', 4); // Move prev n times.
-
-    // Set to nth slide
-    $('.carousel').carousel('set', 4);
 
     // Parallax
     $('.parallax').parallax();
